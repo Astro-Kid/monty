fd1b2c779a8a17ac66692dd5313dcfb2db12a89e
Astro-Kid <bupechiyyana15@gmail.com>
2024-02-01 17:59:24 -0800

    Fun ALX

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - print
+ * @head: stack
+ * @counter: line_numb
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
